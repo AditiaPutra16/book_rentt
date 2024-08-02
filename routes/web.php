5eee00a03e343c58a3c7a8eb28f02c7fddfb98ce
AditiaPutra16 <aditiaputra6907@gmail.com>
2024-08-03 01:38:36 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,14 @@ Route::get('/', function () {
 
 
 Route::middleware('only_guest')->group(function() {
-Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::post('login', [AuthController::class, 'authenticating']);
-Route::get('register', [AuthController::class, 'register']);
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+    Route::post('login', [AuthController::class, 'authenticating']);
+    Route::get('register', [AuthController::class, 'register']);
 });
 
 Route::middleware('auth')->group(function() {
-Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['only_admin']);
-Route::get('profile', [UserController::class, 'profile'])->middleware('only_client');
-Route::get('books', [BookController::class, 'index']);
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['only_admin']);
+    Route::get('profile', [UserController::class, 'profile'])->middleware('only_client');
+    Route::get('books', [BookController::class, 'index']);
 });
