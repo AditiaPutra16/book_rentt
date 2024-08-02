50e4d423bcecabb1248b89849b4ac057b906a627
AditiaPutra16 <aditiaputra6907@gmail.com>
2024-08-03 01:29:45 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 
@@ -20,10 +21,15 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 
+
+Route::middleware('only_guest')->group(function() {
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'authenticating']);
 Route::get('register', [AuthController::class, 'register']);
+});
 
-Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'only_admin']);
-Route::get('profile', [UserController::class, 'profile'])->middleware('auth', 'only_client');
-
+Route::middleware('auth')->group(function() {
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['only_admin']);
+Route::get('profile', [UserController::class, 'profile'])->middleware('only_client');
+Route::get('books', [BookController::class, 'index']);
+});
