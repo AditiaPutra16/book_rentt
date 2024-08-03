a3644cb00d188ba2e4337ebc218952bcb85c2ab6
AndiAsyraf03 <andiasyiraf03@gmail.com>
2024-08-03 19:29:47 +0700

    add

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,66 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat laborum inventore quaerat id nihil atque consequuntur quasi nisi officia dolor aut qui quo accusamus similique in dignissimos, nesciunt, beatae magnam.
-    </p>
+
+    <h1>Welcome, {{Auth::user()->username}}</h1>
+
+    <div class="row mt-5">    
+        <div class="col-lg-4">
+            <div class="card-data book">
+                <div class="row">
+                    <div class="col-6"><i class="bi bi-journal-bookmark"></i></div>
+                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                        <div class="card-desc">Books</div>
+                        <div class="card-count">{{$book_count}}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4">
+            <div class="card-data category">
+                <div class="row">
+                    <div class="col-6"><i class="bi bi-list-ul"></i></div>
+                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                        <div class="card-desc">Categories</div>
+                        <div class="card-count">{{$category_count}}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4">
+            <div class="card-data user">
+                <div class="row">
+                    <div class="col-6"><i class="bi bi-people-fill"></i></div>
+                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                        <div class="card-desc">Users</div>
+                        <div class="card-count">{{$user_count}}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="mt-5">
+        <h2>Rent Log</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>User</th>
+                    <th>Book Title</th>
+                    <th>Rent Date</th>
+                    <th>Return Date</th>
+                    <th>Actual Return Date</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td colspan="7" style="text-align: center">No Data</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
