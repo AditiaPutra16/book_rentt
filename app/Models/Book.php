f885d73c6cb0af0387afb6ea7ae53d55c3b230d8
fathanzys <fathanamanda12@gmail.com>
2024-08-04 13:24:07 +0700

    book add

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,9 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+    protected $fillable = [
+        'book_code', 'title', 'cover', 'slug'
+    ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::middleware('auth')->group(function() {
     
     Route::get('books', [BookController::class, 'index']);
     Route::get('book-add', [BookController::class, 'add']);
+    Route::get('book-add', [BookController::class, 'store']);
     
     Route::get('categories', [CategoryController::class, 'index']);
     Route::get('category-add', [CategoryController::class, 'add']);
