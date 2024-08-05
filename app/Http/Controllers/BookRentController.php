4cb5732414515d2f5ba04980e1bcd6c4fda0680e
fathanzys <fathanamanda12@gmail.com>
2024-08-05 23:07:43 +0700

    new

---
+++ app/Http/Controllers/BookRentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class BookRentController extends Controller
+{
+     public function index()
+     {
+        $users = User::where('id','!=', 1)->get();
+        $books = Book::all();
+        return view ('book-rent', [ 'users' => $users, 'books' => $books]);
+     }
+
+     public function store(Request $request)
+     {
+         $request['rent_date'] = Carbon::now()->toDateString();
+         $request['return_date'] = Carbon::now()->addDay(3)->toDateString();
+
+         
+     }
+}
