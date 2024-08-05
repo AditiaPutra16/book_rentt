664c9feb41949f80add33b13d819aa9882582688
AditiaPutra16 <aditiaputra6907@gmail.com>
2024-08-05 13:06:42 +0700

    aa

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,4 +39,32 @@ class UserController extends Controller
         return redirect('user-detail/'.$slug)->with('status','User Approved Successfully');
     }
 
+    public function delete($slug)
+    {
+        $user = User::where('slug', $slug)->first();
+        return view('user-delete',['user' => $user]);
+    }
+
+    public function destroy($slug)
+    {
+        $user = User::where('slug', $slug)->first();
+        $user->delete();
+
+        return redirect('users')->with('status','User Deleted Successfully');
+    }
+
+    public function bannedUsers()
+    {
+        $bannedUsers = User::onlyTrashed()->get();
+        return view('user-banned', ['bannedUsers' => $bannedUsers]);
+    }
+
+    public function restore($slug)
+    {
+        $user = User::withTrashed()->where('slug', $slug)->first();
+        $user->restore();
+
+        return redirect('users')->with('status','User Restored Successfully');
+    }
+
 }
--- resources/views/category-delete.blade.php
+++ resources/views/category-delete.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <h2>are you sure to delete category {{$category->name}}</h2>
+    <h2>Are you sure to delete category {{$category->name}}</h2>
     <div class="mt-5">
         <a href="/category-destroy/{{$category->slug}}" class="btn btn-danger me-3">Sure</a>
         <a href="/categories" class="btn btn-primary">Cancel</a>
